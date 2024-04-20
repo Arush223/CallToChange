201f3e0e6182c89ef9f866c333752aa5e0dcb549
Aarush <aarushj1300@gmail.com>
2024-04-20 14:56:00 -0700

    frontend commit v0.96

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
-'use client'
-import React, { useEffect, useState } from 'react';
+'use client';
+
+import React, { useEffect, useState, useRef } from 'react';
 import NavLinks from './nav-links';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -8,16 +9,19 @@ function Page() {
   const [isVisible, setIsVisible] = useState(false);
   const [percentage, setPercentage] = useState(0);
   const [showText, setShowText] = useState(false);
+  const percentageRef = useRef(0);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsVisible(true);
     }, 0);
 
-    let percentageTimer = setInterval(() => {
-      if (percentage < 50) {
-        setPercentage((prevPercentage) => prevPercentage + 1);
-      } else {
+    const percentageTimer = setInterval(() => {
+      if (percentageRef.current < 55) {
+        percentageRef.current += 1;
+        setPercentage(percentageRef.current);
+      }
+      else {
         clearInterval(percentageTimer);
         setShowText(true);
       }
@@ -27,45 +31,44 @@ function Page() {
       clearTimeout(timer);
       clearInterval(percentageTimer);
     };
-  }, [percentage]);
+  }, []); 
 
   return (
-    <div className="relative h-screen bg-black">
+    <div className="relative h-screen bg-black overflow-hidden">
       <NavLinks />
-      <div className={`absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${isVisible ? 'opacity-100 transition-opacity duration-1000 delay-500' : 'opacity-0'} animationContainer`}>
-        <h1 className={`text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 title`}>
-          Help us combat Carbon Emissions. {percentage}% 
+      <div className={`absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/6 ${isVisible ? 'opacity-100 transition-opacity duration-1000 delay-500' : 'opacity-0'} animationContainer`}>
+        <h1 className={`text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 title overflow-visible`}>
+          Over {percentage}% of companies use LLMs.  
         </h1>
         {showText && (
-          <div className="mt-4">
+          <div className="mt-5">
             <p className={`text-lg font-semibold text-white subtitle`}>
-              <Link legacyBehavior href="/about">
-                <a className="flex items-center space-x-2">
+              <Link href="/about">
+                <span className="flex items-center space-x-1">
                   <span className="opacity-100 transition-opacity duration-1000 delay-1500 hover:text-blue-800">
-                    Learn about our mission
+                    Learn about the environmental cost
                   </span>
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white hover:text-blue-800 transition-colors duration-300" fill="none" viewBox="0 0 20 20" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
                   </svg>
-                </a>
+                </span>
               </Link>
             </p>
           </div>
         )}
 
-        {/* Images Section */}
-        <div className="mt-8 flex justify-center space-x-4">
-          <div className="image-container opacity-0 transition-opacity duration-1000 delay-2000">
-            <Image src="/llm.png" alt="LLM Image" width={128} height={128} />
-            <p className="text-white">LLM Description</p>
+        <div className="mt-16 flex flex-row items-center justify-center space-x-8 ">
+          <div className="flex flex-col items-center space-y-4">
+            <Image src="/llm.png" alt="LLM Image" width={256} height={256} />
+            <p className="text-white mt-2">LLM Description</p>
           </div>
-          <div className="image-container opacity-0 transition-opacity duration-1000 delay-2500">
-            <Image src="/database.png" alt="Database Image" width={128} height={128} />
-            <p className="text-white">Database Description</p>
+          <div className="flex flex-col items-center space-y-4">
+            <Image src="/database.png" alt="Database Image" width={256} height={256}/>
+            <p className="text-white mt-2">Database Description</p>
           </div>
-          <div className="image-container opacity-0 transition-opacity duration-1000 delay-3000">
-            <Image src="/carbon.png" alt="Carbon Image" width={128} height={128} />
-            <p className="text-white">Carbon Description</p>
+          <div className="flex flex-col items-center space-y-4">
+            <Image src="/carbon.png" alt="Carbon Image" width={256} height={256} />
+            <p className="text-white mt-2">Carbon Description</p>
           </div>
         </div>
       </div>
