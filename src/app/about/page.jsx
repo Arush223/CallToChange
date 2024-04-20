3a34895832d9a0cabe3e1f8778b16f42b124ceca
Aarush <aarushj1300@gmail.com>
2024-04-20 16:59:13 -0700

    frontend v0.9705

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -8,19 +8,6 @@ export default function Page() {
       <div className="text-2xl font-bold text-center mt-10">
          CallToChange
       </div>
-      <div className="text-lg text-center mt-5">
-        We are a group of students from UCSD who created this website during LA Hacks.
-      </div>
-      <div className="text-lg text-center mt-5">
-        We are passionate about the environment and wanted to raise awareness about the environmental cost of LLMs.
-      </div>
-      <div className="text-lg text-center mt-5">
-        We hope you enjoy our website!
-      </div>
-      <div className="text-lg text-center mt-5">
-        - Team LiquidDeath
-      </div>
-
       <div className="absolute bottom-0 left-0 p-2 text-white text-xs">
           Copyright &copy; 2024 Team LiquidDeath. Created during LA Hacks.
         </div>
