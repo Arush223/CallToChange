5e99807800e211ba6fc6a27ca203c138746ff7c0
Aarush <aarushj1300@gmail.com>
2024-04-20 15:38:19 -0700

    copyright commit

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -60,6 +60,9 @@ export default function Page() {
         <div>Emissions: {emissions}</div>
         <div>Offset Cost: ${offsetCost}</div>
       </div>
+      <div className="absolute bottom-0 left-0 p-2 text-white text-xs">
+          Copyright &copy; 2024 Team LiquidDeath. Created during LA Hacks.
+        </div>
     </div>
   );
 }
--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -5,6 +5,9 @@ export default function Page() {
   return (
     <div style={{ backgroundColor: 'white', color: 'black' }}>
       <NavLinks />
+      <div className="absolute bottom-0 left-0 p-2 text-white text-xs">
+          Copyright &copy; 2024 Team LiquidDeath. Created during LA Hacks.
+        </div>
     </div>
   );
 }
