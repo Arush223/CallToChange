eb9e038b53b0ddb14562b60f753aa32eff881032
Aarush <aarushj1300@gmail.com>
2024-04-20 16:16:27 -0700

    frontend v0.997

--- src/app/about/layout.jsx
+++ src/app/about/layout.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Layout = ({ children }) => {
   return (
       <div className="flex h-screen" style={{ backgroundColor: 'black', color: 'black' }}>
-        <div className="w-full flex-none md:w-64"></div>
         <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
       </div>
   );
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,8 +3,24 @@ import NavLinks from '../nav-links';
 
 export default function Page() {
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }}>
+    <div style={{ backgroundColor: 'black', color: 'white' }}>
       <NavLinks />
+      <div className="text-2xl font-bold text-center mt-10">
+         CallToChange
+      </div>
+      <div className="text-lg text-center mt-5">
+        We are a group of students from UCSD who created this website during LA Hacks.
+      </div>
+      <div className="text-lg text-center mt-5">
+        We are passionate about the environment and wanted to raise awareness about the environmental cost of LLMs.
+      </div>
+      <div className="text-lg text-center mt-5">
+        We hope you enjoy our website!
+      </div>
+      <div className="text-lg text-center mt-5">
+        - Team LiquidDeath
+      </div>
+
       <div className="absolute bottom-0 left-0 p-2 text-white text-xs">
           Copyright &copy; 2024 Team LiquidDeath. Created during LA Hacks.
         </div>
