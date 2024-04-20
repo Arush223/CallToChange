4a11fd5219f8026ca33badb961687b3a01213a27
Aditya Kakarla <adityakakarla@Adityas-MacBook-Air.local>
2024-04-20 12:37:21 -0700

    updated server actions

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,8 +1,11 @@
 'use server'
 
 import { getUserInfo } from "../../scripts/mongo/mongo_scripts"
+import { currentUser } from "@clerk/nextjs/server";
 
-export async function fetchUserInfo(email){
+export async function fetchUserInfo(){
+    const user = await currentUser();
+    const email = user && user.emailAddresses.length > 0 ? user.emailAddresses[0].emailAddress : null;
     const result = await getUserInfo(email);
-    return result;
+    return result; // returns an object containing {text_count: int, image_count: int}
 }
