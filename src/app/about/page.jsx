3b62cd38c38edd2533c393a697b395054fbf3295
Aditya Kakarla <adityakakarla@Adityas-MacBook-Air.local>
2024-04-21 00:57:52 -0700

    final ui update

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/no-unescaped-entities */
-'use client'
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
-import NavLinks from '../nav-links';
+"use client";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import NavLinks from "../nav-links";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function Page() {
   const [isVisible, setIsVisible] = useState(false);
   const [showText, setShowText] = useState(false);
   const [activeSection, setActiveSection] = useState(null);
-
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -27,50 +27,107 @@ export default function Page() {
 
   const sections = [
     {
-      title: 'Our Python Package',
-      content: 'Our Python package call-to-change helps you log text and image generation calls. For more info, check out the repo: ',
-      link: 'https://github.com/adityakakarla/call-to-change'
+      title: "Our Python Package",
+      content:
+        "Our Python package call-to-change helps you log text and image generation calls. For more info, check out the repo: ",
+      link: "https://github.com/adityakakarla/call-to-change",
     },
     {
-      title: 'Emissions Calculators',
-      content: 'We calculate emissions based on the number of text generation calls and image generation calls. Using government and academic formulas, we calculate the cost to offset the emissions.',
+      title: "Emissions Calculators",
+      content:
+        "We calculate emissions based on the number of text generation calls and image generation calls. Using government and academic formulas, we calculate the cost to offset the emissions.",
     },
     {
-      title: 'Fancy Dashboards',
-      content: 'Our dashboards show you information at every step of the process, including text generation calls, image generation calls, emissions generated, and the cost to offset emissions.',
+      title: "Fancy Dashboards",
+      content:
+        "Our dashboards show you information at every step of the process, including text generation calls, image generation calls, emissions generated, and the cost to offset emissions.",
     },
   ];
 
   return (
     <div className="relative h-screen bg-black text-white text-left overflow-hidden">
       <NavLinks />
-      <div className={`absolute p-5 top-0 left-1/2 transform -translate-x-1/2 ${isVisible ? 'opacity-100 transition-opacity duration-1000 delay-500' : 'opacity-0'} animationContainer grid place-items-center`}>
+      <div
+        className={`absolute p-5 top-0 left-1/2 transform -translate-x-1/2 ${
+          isVisible
+            ? "opacity-100 transition-opacity duration-1000 delay-500"
+            : "opacity-0"
+        } animationContainer grid place-items-center`}
+      >
         <h1 className="text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 title overflow-visible p-2 mt-20 mb-10">
           <span className="inline-block align-middle">CallToChange</span>
         </h1>
-        <p className={`text-center p-2 text-lg font-semibold text-white animate-slide-up mb-8 ${showText ? 'opacity-100 transition-opacity duration-2000 delay-1500' : 'opacity-0'}`}>
-            A Python-based Carbon Emission Calculator for a company's LLM calls.
+        <p
+          className={`text-center p-2 text-lg font-semibold text-white animate-slide-up mb-8 ${
+            showText
+              ? "opacity-100 transition-opacity duration-2000 delay-1500"
+              : "opacity-0"
+          }`}
+        >
+          A Python-based Carbon Emission Calculator for a company's LLM calls.
         </p>
-        <div className={`w-full max-w-2xl animate-slide-up ${showText ? 'opacity-100 transition-opacity duration-2000 delay-1500' : 'opacity-0'}`}>
-              <ul className="space-y-4 w-700">
-                {sections.map((section, index) => (
-                  <li key={index} className="border-b border-gray-700">
-                    <button
-                      className="w-full text-left text-lg font-semibold text-white bg-transparent p-4 focus:outline-none focus-visible:ring focus-visible:ring-opacity-50"
-                      onClick={() => setActiveSection(activeSection === index ? null : index)}
-                    >
-                      {section.title}
-                    </button>
-                    {activeSection === index && (
-                      <div className="p-4 text-white">
-                        {section.content}
-                        {section.link && <a href={section.link} className='text-white hover:text-blue-300 transition-colors duration-300 cursor-pointer'>{section.link}</a>}
-                      </div>
+        <div
+          className={`w-full max-w-2xl animate-slide-up ${
+            showText
+              ? "opacity-100 transition-opacity duration-2000 delay-1500"
+              : "opacity-0"
+          }`}
+        >
+          <ul className="space-y-4 w-700">
+            {sections.map((section, index) => (
+              <li key={index} className="border-b border-gray-700">
+                <button
+                  className="w-full text-left text-lg font-semibold text-white bg-transparent p-4 focus:outline-none focus-visible:ring focus-visible:ring-opacity-50"
+                  onClick={() =>
+                    setActiveSection(activeSection === index ? null : index)
+                  }
+                >
+                  {section.title}
+                </button>
+                {activeSection === index && (
+                  <div className="p-4 text-white">
+                    {section.content}
+                    {section.link && (
+                      <a
+                        href={section.link}
+                        className="text-white hover:text-blue-300 transition-colors duration-300 cursor-pointer"
+                      >
+                        {section.link}
+                      </a>
                     )}
-                  </li>
-                ))}
-              </ul>
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
+          <div className="flex justify-center mt-10 text-2xl">
+          <SignedIn>
+            <div className={`text-center p-2 text-lg font-semibold text-white `}>
+            <Link href="/dashboard" className="text-white hover:text-blue-300 transition-colors duration-300 cursor-pointer">
+              <span className="text-underline">
+                Try it out here.
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-300 hover:text-green-400 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+                </svg>
+              </span>
+            </Link>
             </div>
+          </SignedIn>
+          <SignedOut>
+          <div className={`text-center p-2 text-lg font-semibold text-white`}>
+            <Link href="/auth/sign-up" className="text-white hover:text-blue-300 transition-colors duration-300 cursor-pointer">
+              <span className="text-underline">
+                Try it out here.
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-300 hover:text-green-400 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+                </svg>
+              </span>
+            </Link>
+            </div>
+          </SignedOut>
+          </div>
+
+        </div>
       </div>
     </div>
   );
