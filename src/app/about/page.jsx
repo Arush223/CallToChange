808aabec979e525af5fcc5bfed90c08d05a25557
Aditya Kakarla <adityakakarla@Adityas-MacBook-Air.local>
2024-04-21 00:35:03 -0700

    updated aarushs ui

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -45,14 +45,14 @@ export default function Page() {
     <div className="relative h-screen bg-black text-white text-left overflow-hidden">
       <NavLinks />
       <div className={`absolute p-5 top-0 left-1/2 transform -translate-x-1/2 ${isVisible ? 'opacity-100 transition-opacity duration-1000 delay-500' : 'opacity-0'} animationContainer grid place-items-center`}>
-        <h1 className="text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 title overflow-visible p-2">
+        <h1 className="text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 title overflow-visible p-2 mt-20 mb-10">
           <span className="inline-block align-middle">CallToChange</span>
         </h1>
-        <p className={`text-center p-2 text-lg font-semibold text-white animate-slide-up ${showText ? 'opacity-100 transition-opacity duration-2000 delay-1500' : 'opacity-0'}`}>
+        <p className={`text-center p-2 text-lg font-semibold text-white animate-slide-up mb-8 ${showText ? 'opacity-100 transition-opacity duration-2000 delay-1500' : 'opacity-0'}`}>
             A Python-based Carbon Emission Calculator for a company's LLM calls.
         </p>
-        <div className="w-full max-w-2xl">
-              <ul className="space-y-4">
+        <div className={`w-full max-w-2xl animate-slide-up ${showText ? 'opacity-100 transition-opacity duration-2000 delay-1500' : 'opacity-0'}`}>
+              <ul className="space-y-4 w-700">
                 {sections.map((section, index) => (
                   <li key={index} className="border-b border-gray-700">
                     <button
